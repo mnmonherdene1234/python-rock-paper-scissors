c55e5be6a9edbb523196dc6e71420a0d8833c498
mnmonherdene1234 <mnmonherdene1234@gmail.com>
2023-07-14 17:24:42 +0800

    main async

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+import asyncio
 import pygame
 import sys
 import scenes.scenes_manager as sm
 from assets.images import game_icon
 
-pygame.init()
 
-screen = pygame.display.set_mode((600, 600), pygame.FULLSCREEN)
-pygame.display.set_caption("Rock, Paper, Scissors Game")
-pygame.display.set_icon(game_icon)
+async def main():
+    pygame.init()
 
-sm.init(screen)
+    screen = pygame.display.set_mode((600, 600))
+    pygame.display.set_caption("Rock, Paper, Scissors Game")
+    pygame.display.set_icon(game_icon)
 
-while sm.running:
-    sm.update()
-    pygame.display.flip()
+    sm.init(screen)
 
-pygame.quit()
-sys.exit()
+    while sm.running:
+        sm.update()
+        pygame.display.flip()
+        await asyncio.sleep(0)
+
+    pygame.quit()
+    sys.exit()
+
+
+asyncio.run(main())
